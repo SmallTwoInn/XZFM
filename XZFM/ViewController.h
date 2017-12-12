d869138bec49b6fbd1265d23df806f44c74d48dc
up_learn <up_learn@sina.com>
2017-12-12 15:27:04 +0800

    初始化项目

---
+++ XZFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZFM
+//
+//  Created by Mac on 2017/12/12.
+//  Copyright © 2017年 陆yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XZFM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XZFM
+//
+//  Created by Mac on 2017/12/12.
+//  Copyright © 2017年 陆yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+}
+
+
+@end
